c0ff3738033666feca8075ab005b3ed099dec28c
AlbRayon <alberto_raygut@outlook.com>
2024-07-08 18:41:36 -0600

    Se puede editar archivos

--- src/main/java/gm/inventarios/controlador/ProductoControlador.java
+++ src/main/java/gm/inventarios/controlador/ProductoControlador.java
@@ -1,15 +1,14 @@
 package gm.inventarios.controlador;
 
+import gm.inventarios.excepcion.RecursoNoEncontradoExcepcion;
 import gm.inventarios.modelo.Producto;
 import gm.inventarios.servicio.ProductoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,8 +26,39 @@ public class ProductoControlador {
     public List<Producto> obtenerProductos(){
         List<Producto> productos = this.productoServicio.listarProductos();
         logger.info("Productos obtenidos");
+        logger.info("Se esta imprimiendo en la consola de TomCat");
         productos.forEach((producto -> logger.info(producto.toString())));
         return productos;
     }
 
+    @PostMapping("/productos")
+    public  Producto agregarProducto(@RequestBody Producto producto){
+        logger.info("Producto a agregar: "+ producto);
+        return this.productoServicio.guardarProducto(producto);
+
+    }
+
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> obtenerProductoPorId(@PathVariable int id){
+        Producto producto = this.productoServicio.buscarProductoPorId(id);
+        if(producto != null)
+            return ResponseEntity.ok(producto);
+        else
+            throw new RecursoNoEncontradoExcepcion("No se encontro el id: "+ id);
+        }
+
+    @PutMapping("/productos/{id}")
+    public ResponseEntity<Producto> actualizarProducto(@PathVariable int id, @RequestBody Producto productoRecibido){
+        Producto producto = this.productoServicio.buscarProductoPorId(id);
+        producto.setDescripcion(productoRecibido.getDescripcion());
+        producto.setPrecio(productoRecibido.getPrecio());
+        producto.setExistencia(productoRecibido.getExistencia());
+        this.productoServicio.guardarProducto(producto);
+        return ResponseEntity.ok(producto);
+    }
+
 }
+
+
+
+
